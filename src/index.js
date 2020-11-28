444e8dbe4df55bb8bd61216345bba5542980b610
unknown <siddartharyan180@gmail.com>
2020-11-28 19:30:26 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,44 +1,64 @@
 const express = require('express')
 const app = express()
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/covidtally").then(() => console.log('connected to MongoDb')).catch((err) => console.log('exception', err));
 const port = 8080
 
 // Parse JSON bodies (as sent by API clients)
 let data = require('./data.js');
+
 data = data['data'];
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 const { connection } = require('./connector')
-    //console.log(data);
-app.get('/totalRecovered', (req, res) => {
-    let ans = 0;
-    for (let i = 0; i < data.length; i++) {
-        let k = data[i];
-        ans += Number(k['recovered']);
-    }
-    res.json({ data: { _id: 'total', recovered: ans } });
+
+const schema = new mongoose.Schema({
+    state: String,
+    infected: Number,
+    recovered: Number,
+    death: Number
 })
 
-app.get('/totalActive', (req, res) => {
+
+function round(value) {
+    var decimals = 5;
+    var isNegative = value < 0;
+    var absolute = Math.round(
+        Math.round(
+            Math.abs(value) * Math.pow(10, (6))
+        ) / Math.pow(10, 6 - decimals)
+    ) / Math.pow(10, decimals);
+    return isNegative ? -absolute : absolute;
+}
+const Covid = mongoose.model('covidtallies', schema);
+
+//console.log(data);
+const getValue = (ans, param) => {
+    let k = ans.reduce((acc, value) => acc + Number(value[param]), 0)
+    return k;
+}
+
+const getActive = (ans) => {
     let infected = 0;
     let recovered = 0;
-    for (let i = 0; i < data.length; i++) {
-        let k = data[i];
+    for (let i = 0; i < ans.length; i++) {
+        let k = ans[i];
         recovered += Number(k['recovered']);
         infected += Number(k['infected']);
     }
-    res.json({ data: { _id: 'total', active: (infected - recovered) } });
+    return infected - recovered;
+}
+app.get('/totalRecovered', (req, res) => {
+    Covid.find({}).then((ans) => res.json({ data: { _id: "total", recovered: getValue(ans, 'recovered') } }))
+})
+app.get('/totalActive', (req, res) => {
+    Covid.find({}).then((ans) => res.json({ data: { _id: "total", active: getActive(ans) } }));
 })
 
 app.get('/totalDeath', (req, res) => {
-    let dead = 0;
-    for (let i = 0; i < data.length; i++) {
-        let k = data[i];
-        dead += Number(k['death']);
-    }
-    res.json({ data: { _id: 'total', death: dead } });
+    Covid.find({}).then((ans) => res.json({ data: { _id: 'total', death: getValue(ans, 'death') } }));
 })
-
 
 app.get('/hotspotStates', (req, res) => {
     let dead = [];
@@ -48,7 +68,7 @@ app.get('/hotspotStates', (req, res) => {
         let recovered = k['recovered'];
         let diff = Number(infected) - Number(recovered);
         diff = (diff / Number(infected));
-        diff = diff.toFixed(5);
+        diff = round(diff);
         console.log(diff);
         if (diff > 0.1) {
             dead.push({ state: k['state'], rate: diff });
@@ -64,7 +84,7 @@ app.get('/healthyStates', (req, res) => {
         let infected = k['infected'];
         let death = k['death'];
         let diff = (Number(death) / Number(infected));
-        diff = diff.toFixed(5);
+        diff = round(diff);
         if (diff < 0.005) {
             dead.push({ state: k['state'], mortality: diff });
         }
